58add6a1b71f28ba1fcfa12e8f30d805da326c7d
Ivan Delgado <ivan.delgado@lastminute.com>
2023-11-06 06:51:29 +0100

    wip

--- src/main/kotlin/domain/payment/data/paymentaccount/PaymentAccount.kt
+++ src/main/kotlin/domain/payment/data/paymentaccount/PaymentAccount.kt
@@ -2,7 +2,8 @@ package domain.payment.data.paymentaccount
 
 data class PaymentAccount
 (
-    val accountId: AccountId
+    val accountId: AccountId,
+    val authorisationAction: AuthorisationAction
 )
 
 
